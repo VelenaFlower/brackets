effcf45849d3937490726bf350f2626eaf79181c
Elena Yanchenko <veleno.ajt@gmail.com>
2019-09-30 19:09:42 +0300

    feat: add my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+    let newStr,
+        lastBracket,
+        stack = [];
+
+    newStr = str.split('');
+    newStr.forEach((bracket, index) => {
+        if (index === 0) {
+            stack.push(bracket);
+            return;
+        }
+        lastBracket = bracketsConfig.find(typeBracket => stack[stack.length - 1] === typeBracket[0]);
+        lastBracket = lastBracket && lastBracket[1];
+        if (bracket !== lastBracket) {
+            stack.push(bracket);
+        } else stack.pop();
+    });
+
+    return stack.length === 0;
+};
